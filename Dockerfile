c6614fafc5ce9a14303622bcdaeb627d5a1e89d7
Joshua Tyree <joshuat@createthebridge.com>
2016-03-04 12:32:38 -0600

    Updated

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:latest
+EXPOSE 3000
 
-MAINTAINER Corentin Kerisit "c@42.am"
+MAINTAINER Joshua Tyree "joshuat@createthebridge.com"
 
 RUN echo deb http://ftp.debian.org/debian/ jessie main contrib non-free > /etc/apt/source.list
 
@@ -14,5 +15,10 @@ ADD ./package.json /data/package.json
 RUN cd /data && npm install
 
 ADD . /data/
+
+ENV AWS_ACCESS_KEY_ID AKIAIG32OWLS7TZUARXA
+ENV AWS_SECRET_ACCESS_KEY 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
+ENV S3_BUCKET_NAME blog.ometria.com
+ENV AWS_REGION eu-west-1
 
 CMD node /data/server.js
--- server.js
+++ server.js
@@ -5,20 +5,19 @@ var server = prerender({
 	iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
 	phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
 	messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-    });
+});
 
 
 server.use(prerender.removeScriptTags());
 
-if (process.env.BASIC_AUTH_USERNAME) {
-    server.use(prerender.basicAuth());
-}
+// if (process.env.BASIC_AUTH_USERNAME) {
+//     server.use(prerender.basicAuth());
+// }
 
-server.use(prerender.inMemoryHtmlCache());
-
+// server.use(prerender.inMemoryHtmlCache());
 //server.use(prerender.blacklist());
 //server.use(prerender.httpHeaders());
-// server.use(prerender.s3HtmlCache());
+server.use(prerender.s3HtmlCache());
 // server.use(require('my-plugin'));
 
 server.start();
